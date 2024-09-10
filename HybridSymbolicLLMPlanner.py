12ffbf85a4b9b7d69da381d2d5f1ebeb1db2b582
Helen <helenlu66@gmail.com>
2024-09-09 22:04:02 -0400

    added a function for adding operator to the parsed FOL problem

--- HybridSymbolicLLMPlanner.py
+++ HybridSymbolicLLMPlanner.py
@@ -7,9 +7,11 @@ from tarski.search import GroundForwardSearchModel
 from tarski.search.model import progress
 from tarski.grounding.lp_grounding import ground_problem_schemas_into_plain_operators
 from tarski.io.fstrips import FstripsReader, FstripsWriter
-from tarski.syntax import land, neg
+from tarski.syntax import land, neg, CompoundFormula
 from tarski import fstrips as fs
 from tarski.evaluators.simple import evaluate
+
+from langchain.tools import tool
 
 from utils import *
 
@@ -33,16 +35,119 @@ class HybridSymbolicLLMPlanner:
         problem = self.reader.parse_instance(problem_file_path)
         return problem
     
+    def add_operator(self, operator_name:str, parameters:List[str], precondition:List[str], effects:List[str]):
+        """Adds an action to the current problem
+
+        Args:
+            operator_name (str): the name of the operator
+            parameters (List[str]): a list of parameters. Example: `['?drawer - drawer', '?gripper - gripper']`
+            precondition (List[str]): a list of precondition. Example: `['(not (open ?drawer))', '(free ?gripper)']`
+            effects (List[str]): a list of effects. Example: `['(open ?drawer)']`
+
+        Returns:
+            str: the name of the action added
+        """
+        params_dict = {}
+
+        def parse_parameters(parameters:List[str], params_dict:dict):
+            """fills the params_dict with the mappings from the name of the parameter to the its variable
+
+            Args:
+                parameters (List[str]): list of parameters
+                params_dict (dict): dictionary to store the mappings
+            """
+            for param in parameters:
+                param = param.split(' - ')
+                # first item is the parameter name, second item is the parameter type
+                param_name, param_type_str = param[0], param[1]
+                param_type = self.problem.language.get_sort(param_type_str)
+                param_var = self.problem.language.variable(param_name, param_type)
+                params_dict[param_name] = param_var
+            
+        
+        def parse_predicate(pred:str) -> Tuple[str, List[str], bool]:
+            """parse the predicate into (name, parameter list, negated)
+
+            Args:
+                pred (str): _description_
+                List (_type_): _description_
+                bool (_type_): _description_
+            """
+            
+            parts = pred.replace('(', '').replace(')', '').split()
+            # check if the first part is 'not'
+            if parts[0] == 'not':
+                # if it is, then the second part is the name of the predicate
+                name = parts[1]
+                # the rest of the parts are the arguments
+                args = parts[2:]
+                return name, args, True
+            else:
+                name = parts[0]
+                args = parts[1:]
+                return name, args, False
+
+        def parse_precondition(predicates:List[str]) -> CompoundFormula:
+            """parse the predicates into a CompoundFormula
+
+            Args:
+                predicates (List[str]): list of predicates
+            Returns:
+                CompoundFormula: the compound formula
+            """
+            pred_list = []
+            for pred in predicates:
+                name, args, negated = parse_predicate(pred)
+                args_list = [params_dict[arg] for arg in args]
+                pred = self.problem.language.get_predicate(name)
+                if negated:
+                    pred_list.append(neg(pred(*args_list)))
+                else:
+                    pred_list.append(pred(*args_list))
+            return land(*pred_list)
+
+        def parse_effects(predicates:List[str]) -> List[fs.SingleEffect]:
+            """parse the effects into a list of SingleEffect
+
+            Args:
+                predicates (List[str]): list of predicates
+            Returns:
+                List[fs.SingleEffect]: list of SingleEffect
+            """
+            effects = []
+            for pred in predicates:
+                name, args, negated = parse_predicate(pred)
+                pred = self.problem.language.get_predicate(name)
+                if negated:
+                    effects.append(fs.DelEffect(pred(*[params_dict[arg] for arg in args])))
+                else:
+                    effects.append(fs.AddEffect(pred(*[params_dict[arg] for arg in args])))
+            return effects
+        
+
+        parse_parameters(parameters, params_dict)
+        params_list = list(params_dict.values())
+        precondition_formula:CompoundFormula = parse_precondition(precondition)
+        effects_list:List[fs.SingleEffect] = parse_effects(effects)
+        
+        self.problem.action(
+            name=operator_name,
+            parameters=params_list,
+            precondition=precondition_formula,
+            effects=effects_list
+        )
+        return operator_name
+
     def forward_search(self):
         """performs forward search
         """
-        drawer = self.problem.language.get_sort('drawer')
+        # drawer = self.problem.language.get_sort('drawer')
         
-        drawer_var = self.problem.language.variable('?drawer', drawer)
-        gripper = self.problem.language.get_sort('gripper')
-        gripper_var = self.problem.language.variable('?gripper', gripper)
-        open = self.problem.language.get_predicate('open')
-        free = self.problem.language.get_predicate('free')
+        # drawer_var = self.problem.language.variable('?drawer', drawer)
+        # gripper = self.problem.language.get_sort('gripper')
+        # gripper_var = self.problem.language.variable('?gripper', gripper)
+        # open = self.problem.language.get_predicate('open')
+        # free = self.problem.language.get_predicate('free')
         # self.problem.action(
         #     name='open-drawer',
         #     parameters=[drawer_var, gripper_var],
@@ -215,4 +320,10 @@ def reverse_engineer_plan(node:SearchNode) -> list:
     
 if __name__ == '__main__':
     planner = HybridSymbolicLLMPlanner()
+    planner.add_operator(
+        operator_name='open-drawer',
+        parameters=['?drawer - drawer', '?gripper - gripper'],
+        precondition=['(not (open ?drawer))', '(free ?gripper)'],
+        effects=['(open ?drawer)']
+    )
     planner.forward_search()
