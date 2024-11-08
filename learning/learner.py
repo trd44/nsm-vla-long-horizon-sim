170cb03d2f6068057e470e639b90aabe7f206df4
Helen <helenlu66@gmail.com>
2024-11-08 13:02:59 -0500

    learner config and  method

--- learning/learner.py
+++ learning/learner.py
@@ -1,21 +1,31 @@
 import os
-import stable_baselines3
 import execution
 import execution.executor
 from tarski import fstrips as fs
+from robosuite.robosuite.wrappers import GymWrapper
+from stable_baselines3 import SAC
+from stable_baselines3.common.monitor import Monitor
 
 class Learner:
-    def __init__(self, env, domain:str, grounded_operator_to_learn:fs.Action):
+    def __init__(self, env, domain:str, grounded_operator_to_learn:fs.Action, config:dict):
         self.env = env
         self.domain = domain
         self.grounded_operator = grounded_operator_to_learn
-        
+        self.config = config
+
     def learn(self) -> execution.executor.Executor_RL:
         """Train an RL agent to learn the operator.
 
         Returns:
             execution.executor.Executor_RL: an RL executor for the operator and executes the policy for the operator when called
         """
-        #TODO: implement the training loop
-        pass
+        model = SAC(
+            "MlpPolicy",
+            env = self.env,
+            tensorboard_log=f'learning/policies/{self.domain}/{self.grounded_operator.name}/seed_{self.config["seed"]}/logs',
+            **self.config
+        )
+        model.learn(
+            total_timesteps=self.config['timesteps'])
+        model.save(f'learning/policies/{self.domain}/{self.grounded_operator.name}/seed_{self.config["seed"]}/model')
     
