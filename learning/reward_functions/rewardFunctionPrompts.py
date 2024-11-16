b59c2643a7db75c0fcd185ed68acd030cb03e78b
Helen <helenlu66@gmail.com>
2024-11-16 00:41:17 -0500

    prompt for llm to order operator effects

--- learning/reward_functions/rewardFunctionPrompts.py
+++ learning/reward_functions/rewardFunctionPrompts.py
@@ -1,5 +1,17 @@
 # placeholder prompt for the LLM to order a given operator's effects in the order they are expected to be achieved
-order_effects_prompt = """order the effects of the {operator} in terms of which effects are expected to be achieved before others"""
+order_effects_prompt = """Sort the effects of the grounded operator below in the order they are expected to be achieved. The operator:
+```
+{grounded_operator}
+```
+Output the sorted grounded effects in the following format where the effects are separated by new lines.
+```
+effects:
+(effect1)
+(effect2)
+(effect3)
+...
+```
+"""
 
 # placeholder prompt for the LLM to generate a reward function. 
 reward_shaping_prompt = """generate a python reward function"""
