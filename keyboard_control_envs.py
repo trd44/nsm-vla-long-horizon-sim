39ee3c048d79a4e9bbf160ea0fcf87f2644a1995
Helen Lu <helenlu66@gmail.com>
2025-01-28 16:59:40 -0500

    use keyboard control env to identify convenient placement of the mug in cleanup

--- keyboard_control_envs.py
+++ keyboard_control_envs.py
@@ -17,6 +17,7 @@ from detection.nut_assembly_detector import NutAssemblyDetector
 from robosuite.wrappers import GymWrapper
 from utils import *
 from learning.learner import *
+from learning.custom_gym_wrapper import *
 import numpy as np
 import mimicgen
 
@@ -43,9 +44,7 @@ def choose_mimicgen_environment():
     # keep only envs that correspond to the different reset distributions from the paper
     # only keep envs that end with "Novelty"
     envs = sorted([x for x in all_envs if x[-7:] == "Novelty"])
-    domain = config['planning']['coffee']['domain']
-    lower_case_domain = domain.lower().replace('_', '')
-    envs = [env for env in envs if (lower_case_domain in env.lower() or domain in env) and 'pre_novelty' not in env.lower()]
+    envs = [env for env in envs if 'novelty' in env.lower()]
 
     # Select environment to run
     print("Here is a list of environments in the suite:\n")
@@ -64,6 +63,105 @@ def choose_mimicgen_environment():
     print("Chosen environment: {}\n".format(envs[k]))
     # Return the chosen environment name
     return envs[k]
+
+def choose_op_wrap(domain, env):
+    """Prints out the previously non-existing operators in the plan and returns the selected operator to wrap
+
+    Args:
+        domain (str): The environment domain
+        env (MujocoEnv): The environment to wrap
+
+    Returns:
+        fs.Action: Chosen operator to wrap
+    """
+    plan = load_plan(config['planning'][domain])
+    previous_executors = {}
+    # Select operator to wrap
+    print("Here is a list of operators in the plan:\n")
+    for i, op in enumerate(plan):
+        # check if the operator has a predefined executor. If it does, that means it does not need to be learned
+        executor = load_executor(config, domain, op)
+        if executor is None:
+            print("[{}] {}".format(i, op))
+            break
+        else:
+            previous_executors[op] = executor
+            print(f"[x] Operator {op} has a predefined executor. Skipping...")
+    print()
+    try:
+        s = input("Choose an operator to wrap " + "(enter a number from 0 to {}): ".format(len(plan) - 1))
+        # parse input into a number within range
+        k = min(max(int(s), 0), len(plan))
+    except:
+        k = 0
+        print("Input is not valid. Use {} by default.\n".format(plan[k]))
+    chosen_op = plan[k]
+    gym_env = GymWrapper(env)
+    return CollisionAblatedOperatorWrapper(
+        env=gym_env,
+        grounded_operator=chosen_op,
+        executed_operators=previous_executors,
+        config=config,
+        domain=domain,
+        rl_algo=None,
+        curr_subgoal=None
+    )
+
+def choose_subgoal(op_wrap:OperatorWrapper) -> OperatorWrapper:
+    """Prints out the subgoals in the grounded operator and returns the selected subgoal
+
+    Args:
+        op_wrap (OperatorWrapper): The OperatorWrapper whose subgoal needs to be set
+
+    Returns:
+        the same OperatorWrapper with the selected subgoal set
+    """
+    # select the subgoal for the operator
+    print("Here is a list of subgoals in the grounded operator:\n")
+    for i, effect in enumerate(op_wrap.grounded_operator.effects):
+        print(f"[{i}] {effect.pddl_repr()}")
+    try:
+        s = input("Choose a subgoal " + "(enter a number from 0 to {}): ".format(len(op_wrap.grounded_operator.effects) - 1))
+        # parse input into a number within range
+        k = min(max(int(s), 0), len(op_wrap.grounded_operator.effects))
+    except:
+        k = 0
+        print("Input is not valid. Use {} by default.\n".format(op_wrap.grounded_operator.effects[k].pddl_repr()))
+    op_wrap.curr_subgoal = op_wrap.grounded_operator.effects[k]
+    return op_wrap
+
+def choose_reward_shaping_fn(op_wrap:OperatorWrapper) -> OperatorWrapper:
+    """Prints out the reward shaping functions and returns the selected reward shaping function
+
+    Args:
+        op_wrap (OperatorWrapper): The OperatorWrapper whose subgoal reward shaping functions need to be se
+
+    Returns:
+        the same OperatorWrapper with the selected reward shaping functions set
+    """
+    # select the reward shaping file for each effect in the grounded operator
+    print("Here is a list of reward shaping functions for each effect in the grounded operator:\n")
+    for effect in op_wrap.grounded_operator.effects:
+        print(f"Reward shaping functions for {effect.pddl_repr()}:")
+        for i, file_name in enumerate(os.listdir(f"learning/reward_functions/{op_wrap.domain}")):
+            if "__pycache__" in file_name:
+                continue
+            print(f"[{i}] {file_name}")
+        try:
+            s = input("Choose a reward shaping function for the effect " + "(enter a number from 0 to {}): ".format(len(os.listdir(f"learning/reward_functions/{op_wrap.domain}")) - 1))
+            # parse input into a number within range
+            k = min(max(int(s), 0), len(os.listdir(f"learning/reward_functions/{op_wrap.domain}")))
+        except:
+            k = 0
+            print("Input is not valid. Use {} by default.\n".format(os.listdir(f"learning/reward_functions/{op_wrap.domain}")[k]))
+        # drop the .py extension
+        reward_shaping_fn_file_name = os.listdir(f"learning/reward_functions/{op_wrap.domain}")[k].split(".")[0]
+    
+        reward_shaping_fn_module = importlib.import_module(f"learning.reward_functions.{op_wrap.domain}.{reward_shaping_fn_file_name}")
+        reward_shaping_func = getattr(reward_shaping_fn_module, 'reward_shaping_fn')
+        op_wrap.set_subgoal_reward_shaping_fn(effect, reward_shaping_func)
+        print()
+    return op_wrap
 
 
 if __name__ == "__main__":
@@ -91,18 +189,29 @@ if __name__ == "__main__":
         reward_shaping=True,
         hard_reset=False,
     )
-    # Wrap this environment in a visualization wrapper
-    # config = load_config("config.yaml") # make sure planning is set to the nut_assembly domain
-    # config = config['planning']['coffee']
-    # domain = config['domain']
-    # plan = load_plan(config)
-    # grounded_op = plan[0] # a hack to get the pick-up-nut-from-peg operator
-    # dummy_executed_operators = OrderedDict()
-    visual_env = VisualizationWrapper(env, indicator_configs=None)
-    wrapped_env = GymWrapper(visual_env)
-    # wrapped_env = OperatorWrapper(wrapped_env, grounded_op, dummy_executed_operators, config, domain=domain, rl_algo=PPO, curr_subgoal= record_rollouts=False)
-    wrapped_env.reset()
-    # wrapped_env.viewer.set_camera(camera_id=0)
+    wrapped_env = VisualizationWrapper(env, indicator_configs=None)
+
+    # If options["env_name"] starts with "Co", use the Coffee_Detector
+    if options["env_name"][:2] == "Co":
+        detector = CoffeeDetector(wrapped_env)
+        if "Pre_Novelty" not in options["env_name"]:
+            wrapped_env = choose_op_wrap("coffee", wrapped_env)
+            wrapped_env = choose_subgoal(wrapped_env)
+            wrapped_env = choose_reward_shaping_fn(wrapped_env)
+    elif options["env_name"][:2] == "Cu":
+        detector = CleanupDetector(wrapped_env)
+        if "Pre_Novelty" not in options["env_name"]:
+            wrapped_env = choose_op_wrap("cleanup", wrapped_env)
+            wrapped_env = choose_subgoal(wrapped_env)
+            wrapped_env = choose_reward_shaping_fn(wrapped_env)
+    elif options["env_name"][:2] == "Nu":
+        detector = NutAssemblyDetector(wrapped_env)
+        if "Pre_Novelty" not in options["env_name"]:
+            wrapped_env = choose_op_wrap("nut_assembly", wrapped_env)
+            wrapped_env = choose_subgoal(wrapped_env)
+            wrapped_env = choose_reward_shaping_fn(wrapped_env)
+    else:
+        raise ValueError("Unrecognized environment name: {}".format(options["env_name"]))
 
     # keyboard control
     from robosuite.devices import Keyboard
@@ -120,15 +229,6 @@ if __name__ == "__main__":
     wrapped_env.sim.model.cam_pos[camera_id] = np.array([0, 0, 2])  # Modify these values to set the desired position
     env.sim.model.cam_quat[camera_id] = np.array([0.0, 0, 0, 1])  # Example quaternion
     
-    # If options["env_name"] starts with "Co", use the Coffee_Detector
-    if options["env_name"][:2] == "Co":
-        detector = CoffeeDetector(env)
-    elif options["env_name"][:2] == "Cu":
-        detector = CleanupDetector(env)
-    elif options["env_name"][:2] == "Nu":
-        detector = NutAssemblyDetector(env)
-    else:
-        raise ValueError("Unrecognized environment name: {}".format(options["env_name"]))
 
     while True:
         # Reset the environment
